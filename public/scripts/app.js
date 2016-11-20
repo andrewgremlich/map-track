a11a34e2a368903daba8c172a98365b2e760e6df
andrewgremlich <andrew.gremlich@gmail.com>
2016-11-19 18:29:41 -0700

    Update instructions.  Tracking works!

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,286 +1,242 @@
-(function () {
-    "use strict";
+/*********************
+ * APP GLOBAL VARS
+ *********************/
 
-    /*********************
-     * APP GLOBAL VARS
-     *********************/
-
-    var myLatLng,
-        map,
-        selectors = {
-            "main": document.querySelector("main"),
-            "aside": document.querySelector("aside"),
-            "instructions": document.querySelector("#instructions"),
-            "sessionID": document.querySelector("#sessionID"),
-            "waitScreen": document.querySelector("#waitScreen"),
-            "firstPage": document.querySelector("#first-page"),
-            "game": document.querySelector("#game"),
-            "stop": document.querySelector("#stop"),
-            "runner": document.querySelector("#runners")
-        },
-        refreshIntervalId,
-        sessionToken = (Math.random() * 100000).toFixed(),
-        refr = `session/${selectors.sessionID.innerText}`;
+var myLatLng,
+    map,
+    selectors = {
+        "main": document.querySelector("main"),
+        "aside": document.querySelector("aside"),
+        "instructions": document.querySelector("#instructions"),
+        "sessionID": document.querySelector("#sessionID"),
+        "waitScreen": document.querySelector("#waitScreen"),
+        "firstPage": document.querySelector("#first-page"),
+        "game": document.querySelector("#game"),
+        "stop": document.querySelector("#stop"),
+        "runner": document.querySelector("#runners")
+    },
+    refreshIntervalId,
+    sessionToken = (Math.random() * 100000).toFixed(),
+    refr = `session/${selectors.sessionID.innerText}`;
 
 
-    /***********************
-     * MAP SECTION
-     ***********************/
+/***********************
+ * MAP SECTION
+ ***********************/
 
-    function placeMarker() {
-        let marker = new google.maps.Marker({
-            zoom: 15,
-            position: myLatLng
-        });
+function placeMarker() {
+    var marker = new google.maps.Marker({
+        map: map,
+        position: myLatLng
+    });
+}
 
-        let infowindow = new google.maps.InfoWindow({
-            content: '<h2>Find me before I move!  I am hiding at this spot.</h2>',
-            size: new google.maps.Size(150, 50)
-        });
+function initialize() {
+    let mapProp = {
+        center: myLatLng,
+        zoom: 14
+    };
+    map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
 
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.open(map, marker);
-        });
-
-        marker.setMap(null);
-        marker.setMap(map);
+function showPosition(position) {
+    myLatLng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
     }
 
-    function initialize() {
-        let mapProp = {
-            center: new google.maps.LatLng(myLatLng.lat, myLatLng.lng),
-            zoom: 14,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    }
+    database.ref(`${refr}/location`).set(myLatLng);
+}
 
-    function showPosition(position) {
-        myLatLng = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
+function logRunner(runner, username, sp) {
+    return function () {
+
+        if (username === runner) {
+            if (navigator.geolocation) {
+                console.log("Is runner");
+                navigator.geolocation.getCurrentPosition(sp);
+            } else {
+                console.log("Geolocation is not supported by this browser.");
+            }
+        } else {
+            console.log("Is tracker");
         }
+    }
+}
 
-        database.ref(`${refr}/location`).set(myLatLng);
+function updater() {
+
+    database.ref(`${refr}/runner`).once("value", snap => {
+        let runner = snap.val(),
+            username = localStorage["mapTrackUserName"],
+            runnerUpdater = logRunner(runner, username, showPosition);
+
+        refreshIntervalId = setInterval(runnerUpdater, 5000);
 
         database.ref(`${refr}/location`).on("value", function (snap) {
             myLatLng = snap.val();
+
+            console.log(myLatLng);
 
             console.log("refreshing marker");
 
             placeMarker();
         });
-    }
-    /*
+    })
 
-    Old position updater
+}
 
-    function updatePosition(runner) {
 
-        try {
+/************************************************
+ * Utility functions for page transitions
+ ************************************************/
 
-            let username = localStorage["mapTrackUserName"];
+function transitionToWait(sessionToken, appUpdater) {
+    selectors.firstPage.style.display = "none";
+    selectors.waitScreen.style.display = "block";
 
-            if (username === runner) {
-                if (navigator.geolocation) {
-                    console.log("Is runner");
-                    navigator.geolocation.getCurrentPosition(showPosition);
-                } else {
-                    console.log("Geolocation is not supported by this browser.");
-                }
-            } else {
-                console.log("Is tracker");
+    appUpdater(selectors.runner);
+
+    selectors.sessionID.innerText = sessionToken;
+}
+
+function lateUpdater(refString) {
+
+    return function (partDiv) {
+
+        database.ref(refString).on("value", snap => {
+            var partArray = snap.val();
+
+            partDiv.innerHTML = "";
+
+            for (var i = 0; i < partArray.length; i++) {
+                var text = document.createTextNode(partArray[i]),
+                    para = document.createElement("p");
+
+                para.appendChild(text);
+
+                partDiv.appendChild(para);
             }
-
-        } catch (e) {
-            console.log(e);
-        }
+        })
     }
-    */
 
-    function logRunner(runner) {
-        return function () {
-            try {
+}
 
-                let username = localStorage["mapTrackUserName"];
-                if (username === runner) {
-                    if (navigator.geolocation) {
-                        console.log("Is runner");
-                        navigator.geolocation.getCurrentPosition(showPosition);
-                    } else {
-                        console.log("Geolocation is not supported by this browser.");
-                    }
-                } else {
-                    console.log("Is tracker");
+function randomPick(userArray) {
+
+    let random = Math.random(),
+        amountUser = userArray.length,
+        pick = userArray[Math.floor(random * amountUser)];
+
+    return pick;
+}
+
+function closeSession() {
+    localStorage.removeItem('mapTrackUserName');
+    clearInterval(refreshIntervalId);
+    database.ref(refr).remove();
+}
+
+
+/***************
+ * EVENTS
+ ***************/
+
+document.querySelector("#pickRunner").onclick = e => {
+
+    let runner;
+
+    refr = `session/${selectors.sessionID.innerText}`;
+
+    database.ref(`${refr}/participants`).once("value", snap => {
+        var userArray = snap.val();
+
+        if (userArray) {
+            runner = randomPick(userArray);
+
+            database.ref(refr + '/runner').set(runner)
+        }
+    });
+}
+
+document.querySelector("#start").onclick = e => {
+
+    selectors.waitScreen.style.display = "none";
+    selectors.game.style.display = "block";
+    selectors.stop.style.display = "block";
+
+    updater();
+}
+
+document.querySelector("#startSession").onclick = e => {
+
+    let username = document.querySelector("#name").value,
+        sessionToken = document.querySelector("#joinSession").value,
+        refString = `session/${sessionToken}/participants`,
+        uploaderArray = [];
+
+    localStorage["mapTrackUserName"] = username;
+    uploaderArray.push(username);
+
+    if (sessionToken) {
+
+        database.ref(refString).once("value", snap => {
+
+            var partArray = snap.val();
+            if (partArray) {
+                for (var i = 0; i < partArray.length; i++) {
+                    uploaderArray.push(partArray[i]);
                 }
-
-            } catch (e) {
-                console.log(e);
             }
-        }
-    }
+            database.ref(refString).set(uploaderArray);
 
-    function updater() {
+            var appUpdater = lateUpdater(refString);
 
-        database.ref(`${refr}/runner`).once("value", snap => {
-            var runner = snap.val(),
-                runnerUpdater = logRunner(runner);
-
-            refreshIntervalId = setInterval(runnerUpdater, 5000);
+            transitionToWait(sessionToken, appUpdater);
         })
 
+    } else {
+        document.querySelector("#error").innerText = "You must fill the inputs";
     }
+}
 
+selectors.stop.onclick = e => {
+    selectors.game.style.display = "none";
+    selectors.main.style.display = "block";
+    closeSession();
+};
 
-    /************************************************
-     * Utility functions for page transitions
-     ************************************************/
+selectors.instructions.onclick = e => {
+    e.target.style.display = "none";
+    selectors.main.style.display = "none";
+    selectors.aside.style.display = "block";
+}
 
-    function transitionToWait(sessionToken, appUpdater) {
-        selectors.firstPage.style.display = "none";
-        selectors.waitScreen.style.display = "block";
+document.querySelector("#close").onclick = e => {
+    selectors.aside.style.display = "none";
+    selectors.main.style.display = "block";
+    selectors.instructions.style.display = "block";
+}
 
-        appUpdater(selectors.runner);
+/************************************************
+ * START APPLICATION
+ ************************************************/
 
-        selectors.sessionID.innerText = sessionToken;
+function defaultStart(position) {
+    myLatLng = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
     }
+    initialize();
+    placeMarker();
+}
 
-    function lateUpdater(refString) {
-
-        return function (partDiv) {
-
-            database.ref(refString).on("value", snap => {
-                var partArray = snap.val();
-
-                partDiv.innerHTML = "";
-
-                for (var i = 0; i < partArray.length; i++) {
-                    var text = document.createTextNode(partArray[i]),
-                        para = document.createElement("p");
-
-                    para.appendChild(text);
-
-                    partDiv.appendChild(para);
-                }
-            })
-        }
-
-    }
-
-    function randomPick(userArray) {
-
-        let random = Math.random(),
-            amountUser = userArray.length,
-            pick = userArray[Math.floor(random * amountUser)];
-
-        return pick;
-    }
-
-    function closeSession() {
-        localStorage.removeItem('mapTrackUserName');
-        clearInterval(refreshIntervalId);
-        database.ref(refr).remove();
-    }
-
-
-    /***************
-     * EVENTS
-     ***************/
-
-    document.querySelector("#pickRunner").onclick = e => {
-
-        let runner;
-
-        refr = `session/${selectors.sessionID.innerText}`;
-
-        database.ref(`${refr}/participants`).once("value", snap => {
-            var userArray = snap.val();
-
-            if (userArray) {
-                runner = randomPick(userArray);
-
-                database.ref(refr + '/runner').set(runner)
-            }
-        });
-    }
-
-    document.querySelector("#start").onclick = e => {
-
-        selectors.waitScreen.style.display = "none";
-        selectors.game.style.display = "block";
-        selectors.stop.style.display = "block";
-
-        updater();
-    }
-
-    document.querySelector("#startSession").onclick = e => {
-
-        let username = document.querySelector("#name").value,
-            sessionToken = document.querySelector("#joinSession").value,
-            refString = `session/${sessionToken}/participants`,
-            uploaderArray = [];
-
-        localStorage["mapTrackUserName"] = username;
-        uploaderArray.push(username);
-
-        if (sessionToken) {
-
-            database.ref(refString).once("value", snap => {
-
-                var partArray = snap.val();
-                if (partArray) {
-                    for (var i = 0; i < partArray.length; i++) {
-                        uploaderArray.push(partArray[i]);
-                    }
-                }
-                database.ref(refString).set(uploaderArray);
-
-                var appUpdater = lateUpdater(refString);
-
-                transitionToWait(sessionToken, appUpdater);
-            })
-
-        } else {
-            document.querySelector("#error").innerText = "You must fill the inputs";
-        }
-    }
-
-    selectors.stop.onclick = e => {
-        selectors.game.style.display = "none";
-        selectors.main.style.display = "block";
-        closeSession();
-    };
-
-    selectors.instructions.onclick = e => {
-        e.target.style.display = "none";
-        selectors.main.style.display = "none";
-        selectors.aside.style.display = "block";
-    }
-
-    document.querySelector("#close").onclick = e => {
-        selectors.aside.style.display = "none";
-        selectors.main.style.display = "block";
-        selectors.instructions.style.display = "block";
-    }
-
-    /************************************************
-     * START APPLICATION
-     ************************************************/
-
-    function defaultStart(position) {
-        myLatLng = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        }
-        initialize();
-    }
-
+function initMap() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(defaultStart);
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
+}
 
-    document.querySelector("#sessionToken").innerHTML = sessionToken;
-
-}());
+document.querySelector("#sessionToken").innerHTML = sessionToken;
