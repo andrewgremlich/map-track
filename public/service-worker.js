68267423afa9506faa7567ee1e1759565be0794d
andrewgremlich <andrew.gremlich@gmail.com>
2016-12-01 18:55:19 -0700

    delete fetch call in service worker.

--- public/service-worker.js
+++ public/service-worker.js
@@ -17,21 +17,6 @@ self.addEventListener('install', function (e) {
 	);
 });
 
-// when the browser fetches a URL…
-self.addEventListener('fetch', function (event) {
-	// … either respond with the cached object or go ahead and fetch the actual URL
-	event.respondWith(
-		caches.match(event.request).then(function (response) {
-			if (response) {
-				// retrieve from cache
-				return response;
-			}
-			// fetch as normal
-			return fetch(event.request);
-		})
-	);
-});
-
 self.addEventListener('activate', function (e) {
 	console.log('[ServiceWorker] Activate');
 	e.waitUntil(
